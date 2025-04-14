eb689cc04cadacf009eb03bd8eba003f7fadad17
secur3 <ben.miller@ecfirst.com>
2025-04-14 10:14:30 -0500

    bugs

--- src/trackers/build-trackers.js
+++ src/trackers/build-trackers.js
@@ -17,35 +17,35 @@ if (configArgIndex !== -1 && process.argv[configArgIndex + 1]) {
 
 const configPath = path.resolve(__dirname, `./../../${configFileName}`)
 const config = JSON.parse(fs.readFileSync(configPath, 'utf8'))
-const SharedData = require('./helpers/this.sharedData.js')
+const SharedData = require('./helpers/sharedData.js')
 const sharedData = new SharedData(config)
 
 const Progress = require('progress')
 
-const newDataStats = JSON.parse(fs.readFileSync(`${this.sharedData.config.trackerDataLoc}/crawlStats.json`, 'utf8'))
+const newDataStats = JSON.parse(fs.readFileSync(`${sharedData.config.trackerDataLoc}/crawlStats.json`, 'utf8'))
 let requestsArray = []
 
 let chunk = 0
-while (fs.existsSync(`${this.sharedData.config.trackerDataLoc}/commonRequests-${chunk}.json`)) {
-    const requests = JSON.parse(fs.readFileSync(`${this.sharedData.config.trackerDataLoc}/commonRequests-${chunk}.json`, 'utf8'))
+while (fs.existsSync(`${sharedData.config.trackerDataLoc}/commonRequests-${chunk}.json`)) {
+    const requests = JSON.parse(fs.readFileSync(`${sharedData.config.trackerDataLoc}/commonRequests-${chunk}.json`, 'utf8'))
     requestsArray = requestsArray.concat(requests)
     chunk++
 }
 
 // when reading crawl data from disk the region can be found in the crawl metadata
-if (this.sharedData.config.crawlerDataLoc !== 'postgres') {
-    const crawlMetadata = JSON.parse(fs.readFileSync(`${this.sharedData.config.crawlerDataLoc}/metadata.json`, 'utf8'))
-    this.sharedData.config.regionCode = crawlMetadata.config.regionCode
+if (sharedData.config.crawlerDataLoc !== 'postgres') {
+    const crawlMetadata = JSON.parse(fs.readFileSync(`${sharedData.config.crawlerDataLoc}/metadata.json`, 'utf8'))
+    sharedData.config.regionCode = crawlMetadata.config.regionCode
 }
-const countryCode = this.sharedData.config.regionCode || 'US'
+const countryCode = sharedData.config.regionCode || 'US'
 const crawledSiteTotal = newDataStats.sites
 const summary = {trackers: 0, entities: []}
 
 let requestPageMap = {}
 try {
-    requestPageMap = JSON.parse(fs.readFileSync(`${this.sharedData.config.pageMapLoc}/pagemap.json`, 'utf-8'))
+    requestPageMap = JSON.parse(fs.readFileSync(`${sharedData.config.pageMapLoc}/pagemap.json`, 'utf-8'))
 } catch (e) {
-    if (this.sharedData.config.includePages) {
+    if (sharedData.config.includePages) {
         console.error('Could not load request page map: ', e)
     }
 }
@@ -65,7 +65,7 @@ requestsArray.forEach(newTrackerData => {
 
     // create a new tracker file
     if (!trackers[fileName]) {
-        log(`${chalk.yellow('Create tracker:')} ${newTrackerData.rule} - ${newTrackerData.type} ${fileName}`)
+        log(`${chalk.yellow('Create tracker:')} ${newTrackerData.rule} - ${newTrackerData.type} ${fileName}`, sharedData)
         const tracker = new Tracker(newTrackerData, crawledSiteTotal)
         tracker.addRule(rule)
         tracker.addTypes(newTrackerData.type, newTrackerData.sites)
@@ -74,14 +74,14 @@ requestsArray.forEach(newTrackerData => {
         // add this file so we know we now have an existing entry for this tracker
         trackers[fileName] = tracker
         summary.trackers++
-        if (this.sharedData.config.includePages) {
+        if (sharedData.config.includePages) {
             trackerPageMap[tracker.domain] = [...requestPageMap[rule.rule]]
         }
     } else {
-        log(`${chalk.blue('update tracker')} ${fileName}`)
+        log(`${chalk.blue('update tracker')} ${fileName}`, sharedData)
         trackers[fileName].addRule(rule)
         trackers[fileName].addTypes(newTrackerData.type, newTrackerData.sites)
-        if (this.sharedData.config.includePages) {
+        if (sharedData.config.includePages) {
             trackerPageMap[trackers[fileName].domain].push(...requestPageMap[rule.rule])
         }
     }
@@ -93,31 +93,31 @@ requestsArray.forEach(newTrackerData => {
     bar.tick()
 })
 
-if (!fs.existsSync(`${this.sharedData.config.trackerDataLoc}/domains/${countryCode}`)) {
-    fs.mkdirSync(`${this.sharedData.config.trackerDataLoc}/domains/${countryCode}`)
+if (!fs.existsSync(`${sharedData.config.trackerDataLoc}/domains/${countryCode}`)) {
+    fs.mkdirSync(`${sharedData.config.trackerDataLoc}/domains/${countryCode}`)
 }
 
 for (const [fileName, tracker] of Object.entries(trackers)) {
-    const filePath = `${this.sharedData.config.trackerDataLoc}/domains/${countryCode}/${fileName}`
+    const filePath = `${sharedData.config.trackerDataLoc}/domains/${countryCode}/${fileName}`
     fs.writeFileSync(filePath, JSON.stringify(tracker, null, 4))
 }
 
-if (this.sharedData.config.includePages) {
-    fs.writeFileSync(`${this.sharedData.config.pageMapLoc}/trackerpagemap.json`, JSON.stringify(trackerPageMap, null, 4))
+if (sharedData.config.includePages) {
+    fs.writeFileSync(`${haredData.config.pageMapLoc}/trackerpagemap.json`, JSON.stringify(trackerPageMap, null, 4))
 }
 
 console.log(`Found ${summary.trackers} ${chalk.green("trackers")}`)
 console.log(`Found ${summary.entities.length} ${chalk.green("entities")}`)
 console.log(chalk.green("Done"))
 
-if (!fs.existsSync(`${this.sharedData.config.trackerDataLoc}/build-data/generated/${countryCode}`)) {
-    fs.mkdirSync(`${this.sharedData.config.trackerDataLoc}/build-data/generated/${countryCode}`)
+if (!fs.existsSync(`${sharedData.config.trackerDataLoc}/build-data/generated/${countryCode}`)) {
+    fs.mkdirSync(`${sharedData.config.trackerDataLoc}/build-data/generated/${countryCode}`)
 }
 
-fs.writeFileSync(`${this.sharedData.config.trackerDataLoc}/build-data/generated/${countryCode}/releasestats.txt`, `${summary.trackers} domains\n${summary.entities.length} entities`)
+fs.writeFileSync(`${sharedData.config.trackerDataLoc}/build-data/generated/${countryCode}/releasestats.txt`, `${summary.trackers} domains\n${summary.entities.length} entities`)
 
-function log (msg) {
-    if (this.sharedData.config.verbose) {
+function log (msg, sharedData) {
+    if (sharedData.config.verbose) {
         console.log(msg)
     }
 }
